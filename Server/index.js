cb937ab0d5cebe356d705bee0c12ac62ef409490
siddg1161 <siddharthgupta1161@gmail.com>
2026-06-29 02:52:57 +0530

    Temporarily allow all origins

--- Server/index.js
+++ Server/index.js
@@ -23,10 +23,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
     cors({
-       origin: [
-      "http://localhost:3000",
-      process.env.FRONTEND_URL,
-    ].filter(Boolean),
+       origin: true,
     credentials: true,
     })
 )
